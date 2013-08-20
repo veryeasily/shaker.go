666ef56f1fe14475dfdbe3fa51b37c71b1c2f4ed
elju <moresilenter@gmail.com>
2013-08-20 11:20:08 -0400

    Tightened up code, because yolo

--- shaker.go
+++ shaker.go
@@ -18,30 +18,22 @@ func printJson(f *map[string]interface{}) (str string) {
     str = ""
     for k, v := range *f {
         switch vv := v.(type) {
-        case string:
-            fmt.Println(k, "is string", vv)
-            str = v.(string)
-            break
-        case int:
-            fmt.Println(k, "is int", vv)
-        case []interface{}:
-            fmt.Println(k, "is an array")
-            for i, u := range vv {
-                fmt.Println(i, u)
-                str = u.(string)
-                break
-            }
-            break
-        case map[string]interface{}:
-            fmt.Println(k, "is an object")
-            temp := v.(map[string]interface{})
-            str = printJson(&temp)
-        default:
-            fmt.Println(vv, "is probably an object")
+            case string:
+                fmt.Println(k, "is string", vv)
+                str = v.(string)
+            case []interface{}:
+                fmt.Println(k, "is an array")
+                for i, u := range vv {
+                    fmt.Println(i, u)
+                    str = u.(string)
+                    break
+                }
+            case map[string]interface{}:
+                fmt.Println(k, "is an object")
+                temp := v.(map[string]interface{})
+                str = printJson(&temp)
         }
-        if str != "" {
-            break
-        }
+        if str != "" { break }
     }
     fmt.Println(str)
     return str
@@ -50,34 +42,26 @@ func printJson(f *map[string]interface{}) (str string) {
 
 func main() {
     var words = []string{"friendly", "bad"}
-    temp := ""
     if len(os.Args) > 1 {
         words = os.Args[1:]
     }
     str := ""
-    var replacements []string
     length := len(words)
-    replacements = make([]string, length, length)
+    replacements := make([]string, length, length)
     for i, word := range words {
-        temp = word
+        new_word := word
         res, err := http.Get("http://words.bighugelabs.com/api/2/7c1a1031524ef2b6d72070ec9bcf5e5d/" + word + "/json")
-        if err != nil {
-            log.Fatal(err)
-        }
+        if err != nil { log.Fatal(err) }
         contents, err := ioutil.ReadAll(res.Body)
         defer res.Body.Close()
-        if err != nil {
-            log.Fatal(err)
-        }
+        if err != nil { log.Fatal(err) }
         if string(contents) != "" {
             var f map[string]interface{}
             err = json.Unmarshal(contents, &f)
-            if err != nil {
-                log.Fatal(err)
-            }
-            temp = printJson(&f)
+            if err != nil { log.Fatal(err) }
+            new_word = printJson(&f)
         }
-        replacements[i] = temp
+        replacements[i] = new_word
     }
     str = strings.Join(replacements, " ")
     fmt.Println("")
