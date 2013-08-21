c8deb247d088a7da5ee997072835c4efa5091d81
elju <moresilenter@gmail.com>
2013-08-21 12:18:33 -0400

    Added a buggy version, what's up with this?

--- shaker.go
+++ shaker.go
@@ -15,7 +15,6 @@ type JSON struct {
 }
 
 func printJson(f *map[string]interface{}) (str string) {
-    str = ""
     for k, v := range *f {
         switch vv := v.(type) {
             case string:
@@ -35,36 +34,50 @@ func printJson(f *map[string]interface{}) (str string) {
         }
         if str != "" { break }
     }
-    fmt.Println(str)
     return str
 }
 
+type Word struct {
+    word string
+    i int
+}
 
 func main() {
     var words = []string{"friendly", "bad"}
     if len(os.Args) > 1 {
         words = os.Args[1:]
     }
-    str := ""
-    length := len(words)
-    replacements := make([]string, length, length)
+    str, length := "", len(words)
+    c := make(chan Word, length)
     for i, word := range words {
-        new_word := word
-        res, err := http.Get("http://words.bighugelabs.com/api/2/7c1a1031524ef2b6d72070ec9bcf5e5d/" + word + "/json")
-        if err != nil { log.Fatal(err) }
-        contents, err := ioutil.ReadAll(res.Body)
-        defer res.Body.Close()
-        if err != nil { log.Fatal(err) }
-        if string(contents) != "" {
-            var f map[string]interface{}
-            err = json.Unmarshal(contents, &f)
-            if err != nil { log.Fatal(err) }
-            new_word = printJson(&f)
-        }
-        replacements[i] = new_word
+        go getWord(word, i, c)
     }
-    str = strings.Join(replacements, " ")
+    fmt.Println("made it here")
+    x := 0
+    for thing := range c {
+        words[thing.i] = thing.word
+        x++
+        if x == 10 { break }
+    }
+    str = strings.Join(words, " ")
     fmt.Println("")
     fmt.Println("")
     fmt.Println(str)
+}
+
+func getWord(word string, i int, c chan Word) {
+    fmt.Println("Started a goroutine")
+    res, err := http.Get("http://words.bighugelabs.com/api/2/7c1a1031524ef2b6d72070ec9bcf5e5d/" + word + "/json")
+    if err != nil { log.Fatal(err) }
+    contents, err := ioutil.ReadAll(res.Body)
+    defer res.Body.Close()
+    if err != nil { log.Fatal(err) }
+    if string(contents) != "" {
+        var f map[string]interface{}
+        err = json.Unmarshal(contents, &f)
+        if err != nil { log.Fatal(err) }
+        word = printJson(&f)
+    }
+    fmt.Println(word)
+    c <- Word{word, i}
 }
